7e61ec83025079a8d311d33671458a4f2147730b
Facundo Jaton <facundoe.jaton@gmail.com>
2021-04-05 18:06:16 -0300

    Just Readme.md

--- app/src/main/java/com/facundojaton/android_portfolio/domain/interactor/login_interactor/SignInInteractor.kt
+++
@@ -1,7 +0,0 @@
-package com.facundojaton.android_portfolio.domain.interactor.login_interactor
-
-interface SignInInteractor {
-    suspend fun signInWithEmailAndPassword(
-        email: String, password: String
-    )
-}
